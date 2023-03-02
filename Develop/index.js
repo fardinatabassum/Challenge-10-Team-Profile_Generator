4356e557397e590c6198563b5219da5c51ece9f9
fardinatabassum <fardinatabassum@gmail.com>
2023-03-02 15:39:43 -0500

    added all class and employee methods and properties

---
+++ Develop/index.js
@@ -0,0 +1,10 @@
+// Require inquirer
+const inquirer = require('inquirer');
+
+
+const fs = require("fs")
+
+// Require claases
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
